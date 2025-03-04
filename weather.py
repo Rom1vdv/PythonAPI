90dac9e55007d4dbc9ee3f707e14afa745457ca0
Romain <romain.vennet@outlook.com>
2025-03-04 15:49:31 +0100

    updated api

--- weather.py
+++ weather.py
@@ -1,16 +1,23 @@
 import requests
 import json
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+KEY = os.getenv("API_KEYS")
 
 URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
-KEY = "BCYAU98N2ZNJCKNQD68VTMW7M"
-LOCATION = "Brussels"
+LOCATION = "Brussels" # Format is address, partial address, longitude|latitude combo
+DATE_1 = "/2024-01-29" #Format is /yyyy-MM-dd
+DATE_2 = "/2024-01-30"
 UNIT_GROUP = "metric"
 CONTENT_TYPE = "json"
 
 
 
-request = requests.get(f"{URL}{LOCATION}?unitGroup={UNIT_GROUP}&key={KEY}&contentType={CONTENT_TYPE}")
-
+request = requests.get(f"{URL}{LOCATION}{DATE_1}{DATE_2}?unitGroup={UNIT_GROUP}&key={KEY}&contentType={CONTENT_TYPE}")
+print(request)
 jsondata = request.json()
 
 print(json.dumps(jsondata, indent=4), file=open("weather.txt", "w"))
